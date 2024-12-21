a072b9d9c559a206d0229fc3fca50204c8508550
N0C1R <noah.miller7472@gmail.com>
2024-12-21 17:18:32 -0500

    added so that the cart is always displayed when the site loads | moved updateCartQuantity to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,11 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js';
+import {calculateCartQuantity} from '../data/cart.js';
 
 let productsHTML = '';
+
+calculateCartQuantity();
 
 products.forEach((product) => {
   productsHTML += `
@@ -61,22 +64,11 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
-
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
       addToCart(productId);
-      updateCartQuantity();
+      calculateCartQuantity();
     });
   });
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {calculateCartQuantity, cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -111,15 +111,4 @@ document.querySelectorAll('.js-delete-link')
     });
   });
 
-updateCartQuantity();
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector('.js-return-to-home-link')
-    .innerHTML = `${cartQuantity} items`;
-}
+calculateCartQuantity();
